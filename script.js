58cd37853f8e8c37e20803f62c89a69bc9a4591b
Leandro Barbosa <leandroxpg3605@gmail.com>
2024-11-11 17:36:29 -0300

    modo light

--- script.js
+++ script.js
@@ -2,9 +2,19 @@ let btn = document.getElementById('btn-menu');
 let menu = document.getElementById('menu-mobile');
 
 btn.addEventListener('click',()=>{
-    menu.classList.add('abrir_menu')
+    menu.classList.add('abrir_menu');
 })
 
 menu.addEventListener('click',()=>{
-    menu.classList.remove('abrir_menu')
+    menu.classList.remove('abrir_menu');
+})
+
+
+
+let marcador = document.getElementById('marcador');
+let body = document.querySelector('body');
+
+marcador.addEventListener('click',()=>{
+    marcador.classList.toggle('light');
+    body.classList.toggle('light')
 })
