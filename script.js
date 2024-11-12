5b42eb63d0c6eb52435f8c09345c915c37d0dcdb
Leandro Barbosa <leandroxpg3605@gmail.com>
2024-11-12 16:19:58 -0300

    carrossel mobile

--- script.js
+++ script.js
@@ -18,3 +18,40 @@ marcador.addEventListener('click',()=>{
     marcador.classList.toggle('light');
     body.classList.toggle('light')
 })
+
+
+
+const carrossel = document.querySelector('.carrossel');
+const setaEsquerda = document.querySelector('.seta-esquerda');
+const setaDireita = document.querySelector('.seta-direita');
+
+let currentIndex = 0; // Índice do card que está sendo exibido
+const cards = document.querySelectorAll('.card'); // Todos os cards
+const totalCards = cards.length;
+const cardWidth = cards[0].offsetWidth + 70; // Largura de cada card + margem
+
+// Defina a largura total do carrossel (total de cartões)
+carrossel.style.width = `${cardWidth * totalCards}px`;
+
+setaDireita.addEventListener('click', () => {
+    if (currentIndex < totalCards - 1) {
+        currentIndex++;
+    } else {
+        currentIndex = 0; // Volta para o primeiro card
+    }
+    updateCarrossel();
+});
+
+setaEsquerda.addEventListener('click', () => {
+    if (currentIndex > 0) {
+        currentIndex--;
+    } else {
+        currentIndex = totalCards - 1; // Vai para o último card
+    }
+    updateCarrossel();
+});
+
+function updateCarrossel() {
+    const offset = -currentIndex * cardWidth; // Move o carrossel para a esquerda/direita
+    carrossel.style.transform = `translateX(${offset}px)`;
+}
